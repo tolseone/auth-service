dc0728acc792cabe24563750ebb928de42f0ac45
tolseone <asemisynov@mail.ru>
2024-02-28 09:17:02 +0300

    implemented all funcs: SaveUser, UserIsAdmin, App

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -2,15 +2,19 @@ package db
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log/slog"
 	"strings"
 
-	_ "github.com/jackc/pgx/v5/pgconn"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgconn"
 
 	"auth-service/internal/config"
 	"auth-service/internal/domain/models"
+	"auth-service/internal/lib/logger/sl"
+	"auth-service/internal/storage"
 	"auth-service/pkg/client/postgresql"
-
 )
 
 type Storage struct {
@@ -33,21 +37,124 @@ func New(log *slog.Logger) *Storage {
 	}
 }
 
-func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (int64, error) {
+func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (uuid.UUID, error) {
 	const op = "Storage.SaveUser"
-	return 0, nil
+
+	q := `
+		INSERT INTO users (
+			id, 
+			email,
+			password
+		) 
+		VALUES (
+			gen_random_uuid(), 
+			$1, 
+			$2
+		)
+		RETURNING id
+	`
+	s.log.Info(fmt.Sprintf("SQL Query: %s", formatQuery(q)))
+
+	var id string
+
+	if err := s.client.QueryRow(ctx, q, email, passHash).Scan(&id); err != nil {
+		var pgErr *pgconn.PgError
+		if errors.Is(err, pgErr) {
+			pgErr = err.(*pgconn.PgError)
+			newErr := fmt.Errorf(fmt.Sprintf("SQL Error: %s, Detail: %s, Where: %s, Code: %s, SQLState: %s, op: %s", pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState(), op))
+			return uuid.Nil, newErr
+		}
+
+		return uuid.Nil, fmt.Errorf("%s: %w", op, err)
+
+	}
+	UID, err := uuid.Parse(id)
+	if err != nil {
+		s.log.Error("failed to parse UUID", sl.Err(err))
+		return uuid.Nil, fmt.Errorf("%s: %w", op, err)
+	}
+	s.log.Info("Completed to create user")
+
+	return UID, nil
 }
 
 func (s *Storage) User(ctx context.Context, email string) (models.User, error) {
-	return models.User{}, nil
+	const op = "storage.sqlite.User"
+
+	q := `
+        SELECT 
+			id,  
+			email,
+			password
+		FROM users 
+		WHERE 
+			email = $1
+	`
+	s.log.Info(fmt.Sprintf("SQL Query: %s", formatQuery(q)))
+
+	var user models.User
+
+	if err := s.client.QueryRow(ctx, q, email).Scan(&user.ID, &user.Email, &user.PassHash); err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			return models.User{}, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
 }
 
-func (s *Storage) IsAdmin(ctx context.Context, userID int64) (bool, error) {
-	return false, nil
+func (s *Storage) IsAdmin(ctx context.Context, userID uuid.UUID) (bool, error) {
+	const op = "storage.sqlite.IsAdmin"
+
+	q := `
+        SELECT 
+			is_admin
+		FROM users
+		WHERE 
+			id = $1
+	`
+	s.log.Info(fmt.Sprintf("SQL Query: %s", formatQuery(q)))
+
+	var isAdmin bool
+
+	if err := s.client.QueryRow(ctx, q, userID).Scan(&isAdmin); err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			return false, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return isAdmin, nil
 }
 
 func (s *Storage) App(ctx context.Context, appID int) (models.App, error) {
-	return models.App{}, nil
+	const op = "storage.sqlite.App"
+
+	q := `
+        SELECT 
+			id,  
+			name,
+			secret
+		FROM apps
+		WHERE 
+			id = $1
+	`
+	s.log.Info(fmt.Sprintf("SQL Query: %s", formatQuery(q)))
+
+	var app models.App
+
+	if err := s.client.QueryRow(ctx, q, appID).Scan(&app.ID, &app.Name, &app.Secret); err != nil {
+		if errors.Is(err, storage.ErrAppNotFound) {
+			return models.App{}, fmt.Errorf("%s: %w", op, storage.ErrAppNotFound)
+		}
+
+		return models.App{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return app, nil
 }
 
 func formatQuery(q string) string {
